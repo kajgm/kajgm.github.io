022afd4f0a290cf7a30bc4e95579013f9d662e28
Kaj Grant <kajgrant@gmail.com>
2023-01-06 19:01:31 -0800

    fix: minor updates

--- server/index.js
+++ server/index.js
@@ -2,20 +2,20 @@ const path = require('path');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
+const server = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+server.use(express.static(path.resolve(__dirname, '../app/build')));
 
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-
-app.get('/api', (req, res) => {
+server.get('/api', (req, res) => {
     res.json({ message: 'Hello from server!' });
 });
 
-app.listen(PORT, () => {
+// All other GET requests not handled before will return our React app
+server.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../app/build', 'index.html'));
+});
+
+server.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
