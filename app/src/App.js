b6e10e0bfb1c9f824b3d8c37ce72614b1b8422f7
Kaj Grant <kajgrant@gmail.com>
2023-08-18 13:27:56 -0700

    added logos

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,28 @@
-import logo from './logo.png';
+import personalLogo from './logo.png';
+import linkedinLogo from './linkedinlogo.png';
+import githubLogo from './githublogo.png';
+import emailLogo from './emaillogo.avif';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <div className="Main-Content">
-                <div id="Name-Title">
-                    <h1 className="Intro-Text">Kaj Grant-Mathiasen</h1>
-                    <h2 id="About-Text" className="Intro-Text">
+            <div id="logos-container">
+                <img src={personalLogo} id="personal-logo" alt="KGM" />
+                <div id="socials-container">
+                    <img src={linkedinLogo} id="linkedIn-logo" className="socials-logos" alt="LinkedIn" />
+                    <img src={githubLogo} id="github-logo" className="socials-logos" alt="Github" />
+                    <img src={emailLogo} id="email-logo" className="socials-logos" alt="Email" />
+                </div>
+            </div>
+            <div className="main-content">
+                <div id="name-title">
+                    <h1 className="intro-text">Kaj Grant-Mathiasen</h1>
+                    <h2 id="about-text" className="intro-text">
                         Student - Computer Engineer - Software Engineer
                     </h2>
                 </div>
             </div>
-            <img src={logo} id="App-logo" alt="logo" />
         </div>
     );
 }
