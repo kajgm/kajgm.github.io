028d792ad01ea524fcaad8b61f55aaf9fa9e7dd3
Kaj Grant <kajgrant@gmail.com>
2023-08-21 23:48:07 -0700

    minor icon improvements and initial sidebar

--- app/src/App.js
+++ app/src/App.js
@@ -7,26 +7,42 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <div id="logos-container">
-                <img src={personalLogo} id="personal-logo" alt="KGM" />
-                <div id="socials-container">
-                    <a href="mailto:kajgrant@gmail.com">
-                        <img src={emailLogo} id="email-logo" className="socials-logos" alt="Email" />
+            <div id="top-header">
+                <div id="logos-container">
+                    <img src={personalLogo} id="personal-logo" alt="KGM" />
+                    <div id="socials-container">
+                        <a href="mailto:kajgrant@gmail.com">
+                            <img src={emailLogo} id="email-logo" className="socials-logos" alt="Email" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/kaj-grant-mathiasen/">
+                            <img src={linkedinLogo} id="linkedIn-logo" className="socials-logos" alt="LinkedIn" />
+                        </a>
+                        <a href="https://github.com/kajgrant">
+                            <img src={githubLogo} id="github-logo" className="socials-logos" alt="Github" />
+                        </a>
+                    </div>
+                </div>
+
+                <div id="name-title">
+                    <a className="intro-text" href="https://kajgrant.github.io/">
+                        <h1 className="intro-text">Kaj Grant-Mathiasen</h1>
                     </a>
-                    <a href="https://www.linkedin.com/in/kaj-grant-mathiasen/">
-                        <img src={linkedinLogo} id="linkedIn-logo" className="socials-logos" alt="LinkedIn" />
-                    </a>
-                    <a href="https://github.com/kajgrant">
-                        <img src={githubLogo} id="github-logo" className="socials-logos" alt="Github" />
-                    </a>
+                    <p id="subheader-text" className="intro-text">
+                        Student - Computer Engineer - Software Engineer - Videographer - Photographer
+                    </p>
                 </div>
             </div>
             <div className="main-content">
-                <div id="name-title">
-                    <h1 className="intro-text">Kaj Grant-Mathiasen</h1>
-                    <p id="about-text" className="intro-text">
-                        Student - Computer Engineer - Software Engineer
-                    </p>
+                <div className="side-bar">
+                    <h1 className="side-headers" id="about">
+                        About
+                    </h1>
+                    <h1 className="side-headers" id="projects">
+                        Projects
+                    </h1>
+                    <h1 className="side-headers" id="media">
+                        Media
+                    </h1>
                 </div>
             </div>
         </div>
