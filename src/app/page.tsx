e883fd2b4c7e7ffbfbd62af29ab23413fb338412
kajgrant <kajgrant@gmail.com>
2024-08-03 22:11:15 -0700

    small layout change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="flex flex-col">
       <ThemeToggle />
-      <div className="mx-auto mt-5 tablet:mt-40 desktop:mt-80">
+      <div className="mx-auto mt-5 tablet:mt-30 desktop:mt-80">
         <div className="flex flex-wrap ml-5 mr-5 desktop:ml-0 desktop:mr-0">
           <div className="mt-3 w-24 mr-20 mb-12 flex flex-wrap">
             <Logo>KGM</Logo>
