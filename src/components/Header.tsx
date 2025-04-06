7244173a7ff736dd95d44a3490777189e614bd3d
Kaj Grant-Mathiasen <kaj.grantmathiasen@canonical.com>
2025-04-06 17:12:01 -0400

    fix overflow issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Brush } from "./img/Brush";
 
 export function Header() {
   return (
-    <div className="relative">
+    <div className="relative overflow-clip">
       <Link className="peer" href="/">
         <h1 className="z-10 relative text-7xl desktop:text-8xl font-bold pb-2 text-light-text-color hover:[text-shadow:_8px_6px_12px_rgb(0_0_0_/_30%)] dark:text-dark-text-color transition-all duration-500">
           Kaj Grant-Mathiasen
