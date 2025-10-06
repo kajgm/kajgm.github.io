5867ef89794388587d4712f29033c6b98760cc8c
Kaj Grant-Mathiasen <kaj.grantmathiasen@canonical.com>
2025-10-06 12:15:41 -0400

    kubestronaut

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,8 @@ import { Location } from "./Location";
 import { Brush } from "./img/Brush";
 
 export function Header() {
+  const textStyleClass =
+    "text-md font-medium text-light-sub-text-color dark:text-dark-sub-text-color";
   return (
     <div className="relative overflow-clip">
       <Link className="peer" href="/">
@@ -12,12 +14,23 @@ export function Header() {
       </Link>
       <Brush className="z-0 absolute -mt-36 ml-24 rotate-3 transition-all duration-700 opacity-0 dark:desktop:peer-hover:opacity-100 dark:fill-red-900 w-[700px] h-[200px]" />
       <div className="z-10 relative pl-1">
-        <p
-          className="text-md font-medium pb-1 text-light-sub-text-color dark:text-dark-sub-text-color transition duration-500"
-          data-testid="subheader"
-        >
-          Computer Engineer / Cloud Support Engineer / Full-Stack Developer
-        </p>
+        <div className="pb-1">
+          <span className={textStyleClass} data-testid="subheader">
+            Computer Engineer / Cloud Engineer / Full-Stack Developer /{" "}
+          </span>
+          <a
+            className="group transition-all duration-500 ease-in-out"
+            href="https://www.cncf.io/training/kubestronaut/?p=kaj-grant-mathiasen"
+          >
+            <span
+              className={
+                "bg-left-bottom bg-gradient-to-r from-light-sub-text-color to-light-sub-text-color dark:from-dark-sub-text-color dark:to-dark-sub-text-color bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-700 ease-out"
+              }
+            >
+              Kubestronaut
+            </span>
+          </a>
+        </div>
         <Location location="Toronto, ON. Canada" />
       </div>
     </div>
