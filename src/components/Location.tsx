9f60bafb6fdf23caa0823404164b1115a51b4549
Kaj Grant-Mathiasen <kaj.grantmathiasen@canonical.com>
2025-10-11 15:09:46 -0400

    update logo

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -10,15 +10,15 @@ export function Location(props: { location: string }) {
         href="https://www.youtube.com/watch?v=nmCm3jrYR74"
         target="_blank"
       >
-        <a className="flex flex-wrap items-center">
-          <Pin className="w-4 h-4 fill-light-sub-text-color group-hover:fill-red-800 dark:fill-dark-sub-text-color dark:group-hover:fill-red-800 group-hover:transition-color group-hover:duration-500 group-hover:ease-in-out ease-in-out transition duration-500" />
+        <div className="flex flex-wrap items-center">
+          <Pin className="mb-0.5 w-4 h-4 fill-light-sub-text-color group-hover:fill-red-800 dark:fill-dark-sub-text-color dark:group-hover:fill-red-800 group-hover:transition-color group-hover:duration-500 group-hover:ease-in-out ease-in-out transition duration-500" />
           <p
             className="m-0 text-md font-medium pl-1 text-light-sub-text-color dark:text-dark-sub-text-color transform transition duration-500"
             data-testid="location"
           >
             {location}
           </p>
-        </a>
+        </div>
         <p className="text-md font-medium px-1 text-light-sub-text-color dark:text-dark-sub-text-color transition duration-500">
           -
         </p>{" "}
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ export function Logo(props: { children: React.ReactNode }) {
   return (
     <div className="pt-1">
       <Link href="/">
-        <div className="w-24 h-14 relative rounded-xl bg-gradient-to-tr from-red-950 to-red-700">
+        <div className="w-24 h-14 hover:scale-110 [box-shadow:_2px_2px_6px_rgb(0_0_0_/_80%)] relative from-red-900 from-20% via-red-700 via-50% to-violet-800 to-100% rounded-xl transition-all bg-left-bottom hover:bg-right-top bg-gradient-to-tr ease-in-out duration-700 bg-[length:200%_200%]">
           <p
             className="pl-2 pt-2 text-white [text-shadow:_6px_4px_8px_rgb(0_0_0_/_60%)] absolute text-3xl font-pmarker tracking-wider"
             data-testid="logo"
